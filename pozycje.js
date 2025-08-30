b314523d9b13aade9971c21dbe02e0f6c1482f06
XRogo <gamesxrogo@gmail.com>
2025-08-30 02:47:39 +0200

    fdfsfsdf

---
+++ pozycje.js
@@ -0,0 +1,19 @@
+const positions = [
+  [-480, 100],
+  [330, 100],
+  [325, 150],
+  [-120, 165],
+  [-130, 405],
+  [-170, 510],
+  [-200, 505],
+  [-275, 475],
+  [-405, 505],
+  [-485, 440],
+  [-470, 400],
+  [-410, 325],
+  [-460, 265],
+  [-540, 310]
+];
+const lineColor = '#ff7b00ff'; // Kolor linii (możesz zmienić, np. 'blue', '#00FF00')
+const fillColor = 'rgba(255, 115, 0, 0.1)'; // Kolor wypełnienia z 70% przeźroczystością
+const closePath = true; // Zamknij ścieżkę (odpowiada 'w' w Twoim przykładzie)
