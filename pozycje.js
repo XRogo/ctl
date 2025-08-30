8e833ade66b9c8257e907762b436c3b456dbcdde
XRogo <gamesxrogo@gmail.com>
2025-08-30 16:01:03 +0200

    dasdasdas

--- pozycje.js
+++ pozycje.js
@@ -1,19 +1,38 @@
-const positions = [
-  [-480, 100],
-  [-330, 100],
-  [-325, 150],
-  [-120, 165],
-  [-130, 405],
-  [-170, 510],
-  [-200, 505],
-  [-275, 475],
-  [-405, 505],
-  [-485, 440],
-  [-470, 400],
-  [-410, 325],
-  [-460, 265],
-  [-540, 310]
+const polygons = [
+    {
+        points: [
+            [-480, 100],
+            [-330, 100],
+            [-325, 150],
+            [-120, 165],
+            [-130, 405],
+            [-170, 510],
+            [-200, 505],
+            [-275, 475],
+            [-405, 505],
+            [-485, 440],
+            [-470, 400],
+            [-410, 325],
+            [-460, 265],
+            [-540, 310]
+        ],
+        lineColor: '#fa6400ff',
+        fillColor: 'rgba(255, 136, 0, 0.2)',
+        closePath: true,
+        name: 'XRogo',
+        category: 1
+    },
+    {
+        points: [
+            [500, 200],
+            [700, 200],
+            [700, 400],
+            [500, 400]
+        ],
+        lineColor: 'green',
+        fillColor: 'rgba(0, 255, 0, 0.7)',
+        closePath: true,
+        name: 'Obszar 2',
+        category: 2
+    }
 ];
-const lineColor = '#ff7b00ff'; // Kolor linii (możesz zmienić, np. 'blue', '#00FF00')
-const fillColor = 'rgba(255, 115, 0, 0.1)'; // Kolor wypełnienia z 70% przeźroczystością
-const closePath = true; // Zamknij ścieżkę (odpowiada 'w' w Twoim przykładzie)
